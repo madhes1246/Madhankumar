8a69afff01973d6e9f622f8b4f1809e023a6cd88
madhes1246 <v.madheswaran55@gmail.com>
2023-06-12 11:51:30 +0530

    testing second commit

--- app/page.js
+++ app/page.js
@@ -4,7 +4,15 @@ export default function Home() {
   return (
     <div>
       <h1>MadhanKumar</h1>
+      <h2>Protfolio</h2>
+      <div class="container-fluid mt-3">
+        <div class="row">
+          <div class="col-sm-4">.col-sm-4</div>
+          <div class="col-sm-8">.col-sm-8</div>
+        </div>
+      </div>
     </div>
+
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     //   <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
     //     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
