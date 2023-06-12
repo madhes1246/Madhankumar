1a5a05d5e5d2e4bdad4fb63cb41208322b17ad14
madhes1246 <v.madheswaran55@gmail.com>
2023-06-12 13:12:29 +0530

    change icon file

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="./icon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
